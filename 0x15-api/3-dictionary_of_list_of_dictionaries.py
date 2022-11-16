d2a4f4a8f8f50fff5a29e94eb0169e7c3ef7285e
Nick Nyanjui <nicknyanjui@gmail.com>
2022-11-17 01:07:35 +0300

    Add 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,33 +10,28 @@ from sys import argv
 url = "https://jsonplaceholder.typicode.com"
 
 
-def fetch_url(id):
+def fetch_all():
     """ Export information from an API about
         `TODO` list progress to JSON file
     """
-    with requests.get(f"{url}/users") as resp:
-        users = resp.json()
-    print(users)
-    for user in users:
-        print(user.id)
-        with requests.get(f"{url}/users/{user.id}") as resp:
+    newDict = {}
+    for user in requests.get(f"{url}/users").json():
+        with requests.get(f"{url}/users/{user.get('id')}") as resp:
             username = resp.json()['username']
-        print(username)
-        todo_url = f"{url}/todos?userId={user.id}"
+        todo_url = f"{url}/todos?userId={user.get('id')}"
         with requests.get(todo_url) as resp:
             myList = resp.json()
-            newDict = {}
-            with open("todo_all_employees.json", "w", encoding="UTF-8") as f:
-                newList = []
-                for j in myList:
-                    myDict = {}
-                    myDict["username"] = username
-                    myDict["task"] = j["title"]
-                    myDict["completed"] = j["completed"]
-                    newList.append(myDict)
-                newDict[id] = newList
-                json.dump(newDict, f)
+            newList = []
+            for j in myList:
+                myDict = {}
+                myDict["username"] = username
+                myDict["task"] = j["title"]
+                myDict["completed"] = j["completed"]
+                newList.append(myDict)
+            newDict[user.get('id')] = newList
+    with open("todo_all_employees.json", "w", encoding="UTF-8") as f:
+        json.dump(newDict, f)
 
 
 if __name__ == "__main__":
-    fetch_url(argv[1])
+    fetch_all()
