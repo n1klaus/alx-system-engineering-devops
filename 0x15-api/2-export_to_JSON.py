8c8d0984597640b75018a4fc24af5c20217c1ed4
Nick Nyanjui <nicknyanjui@gmail.com>
2022-11-17 00:36:59 +0300

    Add 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """  Using REST API, for a given employee ID,
     returns information about his/her TODO list progress
-    and export data in CSV format.
+    and export data in JSON format.
 """
-from csv import writer
-import json
 import requests
+import json
 from sys import argv
 
 url = "https://jsonplaceholder.typicode.com"
@@ -13,25 +12,27 @@ url = "https://jsonplaceholder.typicode.com"
 
 def fetch_url(id):
     """ Export information from an API about
-        `TODO` list progress to csv file
+        `TODO` list progress to JSON file
     """
     user_url = f"{url}/users/{id}"
     username = ""
     with requests.get(user_url) as resp:
-        username = resp.json()["username"]
+        username = resp.json()['username']
 
     todo_url = f"{url}/todos?userId={id}"
     with requests.get(todo_url) as resp:
         myList = resp.json()
-
-        with open(f"{id}.csv", "w", encoding="UTF-8") as f:
-            w = writer(f)
+        newDict = {}
+        with open(f"{id}.json", "w", encoding="UTF-8") as f:
+            newList = []
             for j in myList:
-                newList = []
-                newList.extend(
-                    (str(j['userId']), str(username), str(j['completed']),
-                     str(j['title'])))
-                w.writerow(newList)
+                myDict = {}
+                myDict["task"] = j["title"]
+                myDict["completed"] = j["completed"]
+                myDict["username"] = username
+                newList.append(myDict)
+            newDict[id] = newList
+            json.dump(newDict, f)
 
 
 if __name__ == "__main__":
