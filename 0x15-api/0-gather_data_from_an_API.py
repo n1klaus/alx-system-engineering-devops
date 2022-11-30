caab498587af993128e65dd515a59b4bd5532c6c
Nick Nyanjui <nicknyanjui@gmail.com>
2022-11-30 08:47:08 +0000

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -19,20 +19,20 @@ def fetch_url(id):
     """
     user_url = "{0}/users/{1}".format(url, id)
     name = ""
-    with requests.get(user_url) as resp:
-        name = resp.json()['name']
+    resp = requests.get(user_url)
+    name = resp.json()['name']
 
     todo_url = "{0}/todos?userId={1}".format(url, id)
-    with requests.get(todo_url) as resp:
-        json = resp.json()
-        fl = filter(lambda x: True if x['completed'] is True else False, json)
-        counter = 0
-        titles = ""
-        for item in fl:
-            counter += 1
-            titles += "\t{}\n".format(item['title'])
-        print("Employee {0} is done with tasks({1}/{2}):\n{3}".
-              format(name, counter, len(json), titles[:-1]))
+    resp = requests.get(todo_url)
+    json = resp.json()
+    fl = filter(lambda x: True if x['completed'] is True else False, json)
+    counter = 0
+    titles = ""
+    for item in fl:
+        counter += 1
+        titles += "\t {}\n".format(item['title'])
+    print("Employee {0} is done with tasks({1}/{2}):\n{3}".
+           format(name, counter, len(json), titles[:-1]))
 
 
 if __name__ == "__main__":
