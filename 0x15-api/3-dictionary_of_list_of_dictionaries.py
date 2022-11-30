ca772317f54d99a3748e643acd86b19c3719eb47
Nick Nyanjui <nicknyanjui@gmail.com>
2022-11-30 13:12:50 +0000

    Add 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,34 +1,36 @@
 #!/usr/bin/python3
-"""  Using REST API, for all employees,
+"""
+    Using REST API, for all employees,
     returns information about their TODO list progress
     and export data in JSON format.
 """
-import requests
 import json
+import requests
 from sys import argv
 
 url = "https://jsonplaceholder.typicode.com"
 
 
 def fetch_all():
-    """ Export information from an API about
+    """
+        Export information from an API about
         `TODO` list progress to JSON file
     """
     newDict = {}
-    for user in requests.get(f"{url}/users").json():
-        with requests.get(f"{url}/users/{user.get('id')}") as resp:
-            username = resp.json()['username']
-        todo_url = f"{url}/todos?userId={user.get('id')}"
-        with requests.get(todo_url) as resp:
-            myList = resp.json()
-            newList = []
-            for j in myList:
-                myDict = {}
-                myDict["username"] = username
-                myDict["task"] = j["title"]
-                myDict["completed"] = j["completed"]
-                newList.append(myDict)
-            newDict[user.get('id')] = newList
+    for user in requests.get("{}/users".format(url)).json():
+        resp = requests.get("{0}/users/{1}".format(url, user.get("id")))
+        username = resp.json()['username']
+        todo_url = "{0}/todos?userId={1}".format(url, user.get("id"))
+        resp = requests.get(todo_url)
+        myList = resp.json()
+        newList = []
+        for j in myList:
+            myDict = {}
+            myDict["username"] = username
+            myDict["task"] = j["title"]
+            myDict["completed"] = j["completed"]
+            newList.append(myDict)
+        newDict[user.get('id')] = newList
     with open("todo_all_employees.json", "w", encoding="UTF-8") as f:
         json.dump(newDict, f)
 
