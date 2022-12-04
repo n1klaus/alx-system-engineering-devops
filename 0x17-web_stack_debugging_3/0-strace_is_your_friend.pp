f7b9a229adffa5bf4577be1cffeb02eccf4c2217
Nick Nyanjui <nicknyanjui@gmail.com>
2022-12-04 13:41:12 +0300

    Add 0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,48 @@
+# Fix Apache server error 500
+exec { 'enable environment variables':
+    provider => shell,
+    command  => '. /etc/apache2/envvars',
+}
+
+package { 'nscd':
+    ensure   => installed,
+    provider => 'apt',
+}
+
+exec { 'start nscd':
+    provider => shell,
+    command  => '/etc/init.d/nscd start; /usr/sbin/nscd -i hosts',
+    require  => Package['nscd'],
+}
+
+exec { 'start resolvconf':
+    provider => shell,
+    command  => '/usr/sbin/service resolvconf restart;/usr/sbin/service resolvconf enable-updates',
+    onlyif   => '/usr/bin/which resolvconf',
+}
+
+package { 'libapache2-mod-fcgid':
+    ensure   => installed,
+    provider => 'apt',
+}
+
+package { 'apache2-suexec-custom':
+    ensure   => installed,
+    provider => 'apt',
+}
+
+exec { 'enable suexec':
+    provider => shell,
+    command  => '/usr/sbin/a2enmod suexec',
+    require  => Package['libapache2-mod-fcgid', 'apache2-suexec-custom'],
+}
+
+exec { 'start mysql':
+    provider => shell,
+    command  => '/etc/init.d/mysql restart',
+}
+
+exec { 'start apache2':
+    provider => shell,
+    command  => '/etc/init.d/apache2 restart',
+}
